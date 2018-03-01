f719a872c2234c05add1a987d1f2ade5a3e129c5
IgorDive <ihar.kazlovich@gmail.com>
2018-03-01 18:29:48 +0300

    first modified src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let counterZero = 0;
+  let i = 1;
+  let simpleNumber = true;
+  let tempBase = base;
+  let dividersArr = [];
+
+  for ( let k = 2; k <= tempBase; k++ ) {
+    while ( !(tempBase % k) ) {
+      tempBase /= k;
+      dividersArr.push(k);
+    }
+  }
+
+  while ( number / Math.pow(dividersArr[dividersArr.length - 1], i) >= 1 ) {
+    counterZero += Math.floor( number / Math.pow(dividersArr[dividersArr.length - 1], i) );
+    i += 1;
+  }
+
+  return counterZero;
 }
